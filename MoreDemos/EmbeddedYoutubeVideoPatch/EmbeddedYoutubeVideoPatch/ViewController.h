f70a171e0ba12adce006e88b328edb5d3dce9c2f
Toni <tonisalae@gmail.com>
2012-02-24 13:48:26 +0100

    Added a delegate method (tsMiniWebBrowserDidDismiss) to be notified when the browser is dismissed.

---
+++ MoreDemos/EmbeddedYoutubeVideoPatch/EmbeddedYoutubeVideoPatch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EmbeddedYoutubeVideoPatch
+//
+//  Created by Toni Sala Echaurren on 24/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
